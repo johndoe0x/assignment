9b123a3ff1f2c49389816afa92e16ec59a9d1bbb
johndoe0x <johndoe0x@protonmail.com>
2023-03-25 23:54:41 +0900

    Amend structure.

--- structures.go
+++ structures.go
@@ -1,16 +1,5 @@
 package main
 
-import "container/ring"
-
-func NewCircleByRingCollection(point int) *ring.Ring {
-	circle := ring.New(point)
-	for i := 1; i <= point; i++ {
-		circle.Value = i
-		circle = circle.Next()
-	}
-	return circle
-}
-
 type Interval struct {
 	start, end int
 }
